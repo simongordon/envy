228f35f94fc94efbcd50986ff51eea5f1ffc549b
Simon Gordon <gordonsimon@hotmail.com>
2017-09-13 20:08:54 +1000

    tryna make it do stuff

--- src/popup.ts
+++ src/popup.ts
@@ -1,12 +1,44 @@
 //asdf
 console.log('asdf');
 
+// class Env {
+
+// }
+
+function addListItem(newItem: string) {
+    var list = document.getElementById('envList');
+    var newListItem = document.createElement('li');
+    newListItem.innerText = newItem;
+    list.appendChild(newListItem);
+}
+
+function addToStorage(newItem: string) {
+    var stuff: string[] = localStorage['stuff'];
+    stuff.concat(newItem);
+}
+
+//problems here
+
+let meemz: string[] = JSON.parse(localStorage['stuff']);
+if (meemz == null || meemz.length == 0) {
+    meemz = ["initial", "yeah"];
+    localStorage['stuff'] = meemz;
+}
+meemz.forEach(element => {
+    addListItem(element)
+});
 
 function doMemes() {
     alert('hi');
 }
 
 document.getElementById('btnAdd').addEventListener("click", function () {
-    doMemes();
+    var textBox = <HTMLInputElement>document.getElementById('txtNew');
+    let newItem = textBox.value;
+    console.log('New item: ' + newItem);
+    addToStorage(newItem);
+    addListItem(newItem);
+
+    textBox.value = "";
 });
 
